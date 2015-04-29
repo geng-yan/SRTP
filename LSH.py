a20fb8cf83ecfd6de90adc1ac967671bc9c9f388
rinley <yangeng0317@gmail.com>
2015-04-29 15:24:23 +0800

    add ransac

--- LSH.py
+++ LSH.py
@@ -3,22 +3,16 @@ Created on Mar 22, 2015
 
 @author: mcdreamy
 '''
-import cv2
 import MySQLdb
 import numpy as np
-from cv2 import imread, imshow
-import json
-
-conn=MySQLdb.connect (
-    host='42.121.114.70',
-    port=3306,
-    user='cccoi',
-    passwd='cccoiers',
-    db='buildingdetect',
-    )
-cur=conn.cursor();
-
-
+#conn=MySQLdb.connect(
+#    host='127.0.0.1',
+#    port='3306',
+#    user='root',
+#    passwd='',
+#    db='srtp',
+#)
+#cur=conn.cursor();
 class LSH(object):
     '''
     classdocs
@@ -29,35 +23,32 @@ class LSH(object):
         '''
         Constructor
         '''
-        self.hash_bits=hash_bits # the dimension of the space to be projected
-        self.input_dim=input_dim # the dimension of the input
-        self.table_num=table_num # the number of the para hash tables
-        self._init_uniform_planes() # init the project transform matrix
-        self._init_storage() 
+        self.hash_bits=hash_bits
+        self.input_dim=input_dim
+        self.table_num=table_num
+        self._init_uniform_planes()
+        self._init_storage()
         
     def _init_storage(self):
         '''
-        Use the dict data structure to store the hash key
+        Description
         '''
         self.hash_tables=[dict() for i in range(self.table_num)]
     def _init_uniform_planes(self):
         '''
-        Init the transform matrix
-        one for each hash table 
+        Description
         '''
-        #fileHandle = open ( 'matrix.txt', 'w' )
         self.uniform_planes=[self._generate_uniform_planes() for _ in xrange(self.table_num)]
-        self.load_matrix()
-    
+        
     def _generate_uniform_planes(self):
         '''
-        randn is based on the uniform distribution
+        Description
         '''
         return np.random.randn(self.hash_bits,self.input_dim)
     
     def _hash(self,plane,input_point):
         '''
-        Calculate the hash value
+        Description
         '''
         try:
             input_point=np.array(input_point)
@@ -65,8 +56,7 @@ class LSH(object):
         except:
             pass
         return "".join(['1' if i>0 else '0' for i in projection])
-
-    def index(self,input_point,extra_data=None,keypointid=1):
+    def index(self,input_point,extra_data=None):
         '''
         Description
         '''
@@ -77,14 +67,9 @@ class LSH(object):
         else:
             value=tuple(input_point)
         for i,table in enumerate(self.hash_tables):
-            ret=self._hash(self.uniform_planes[i], input_point)
-            #table.append(self._hash(self.uniform_planes[i], input_point),value)
-            table.setdefault(ret,[]).append(value)
-            
-            #cur.execute("insert into hashval (keypointid,hashtableid,hashvalue) values ('%d','%d','%s');" %(keypointid,i,ret))
-            #conn.commit()
+            #table.append_val(self._hash(self.uniform_planes[i], input_point),value)
+            table.setdefault(self._hash(self.uniform_planes[i], input_point),[]).append(value)
         #print self.hash_tables[0]
-                        
     def query(self,query_point,num_results=None):
         '''
         Description
@@ -93,57 +78,27 @@ class LSH(object):
             num_results=1;
         candidates=set()
         for i,table in enumerate(self.hash_tables):
-            #print i
-            #print 'done2'
-            #print table
             hash_value=self._hash(self.uniform_planes[i], query_point)
-            #print hash_value
-            cur.execute("select kp.localdescriptor, i.buildingid from keypoint kp inner join image i  on kp.imageid=i.imageid where kp.keypointid in ( select keypointid from hashval where hashtableid=%d and hashvalue=Cast('%s' as binary(105)))" % (i,hash_value))
-            #conn.commit()
-            #[localdescriptors,buildingids]=cur.fetchall()
             #print table.get(hash_value),hash_value
-            #print(table.get(hash_value,[]))
-            x=cur.fetchall()
-            #print type(x)
-            #print x
-            candidates.update(x)
-            #candidates.update([])
-            #candidates.update(table.get(hash_value,[]))
-
-        if len(candidates)!=0:
-            candidates=[(i,self._distance(query_point,json.loads(i[0]))) for i in candidates]
-        #print candidates[:0];
-            candidates.sort(key=lambda x:x[1])
-            return candidates[:num_results]
-        else:
-            return 0
+            #print hash_value
+            candidates.update(table.get(hash_value,[]))
+        candidates=[(i,self._distance(query_point,i[0])) for i in candidates]
+        candidates.sort(key=lambda x:x[1])
+        return candidates[:num_results]
+        
         
         
         
     def _distance(self,x,y):
-        '''
-        Euclidean distance
-        '''
         diff=np.array(x)-y
         return np.sqrt(np.dot(diff,diff))
-
-    #def save_matrix(self):
-    #    print"in"
-    #    print self.table_num
-    #    for i in range(self.table_num):
-    #        np.save(("%d" % i),self.uniform_planes[i]);
-    #    print"done"
+    
             
-    def load_matrix(self):
-        
-        for i in range(self.table_num):
-            self.uniform_planes[i]=np.load("%d.npy" % i).tolist();
-    def load_hashtable(self):
-        cur.execute("select keypointid,hashtableid,hashvalue from hashval");
-        for row in cur.fetchall():
-                self.hash_tables[row[1]].setdefault(row[2],[]).append(row[0])
-                print row[1]
-        
+            
+            
+    
+    
+    
         
         
         
--- test.py
+++ test.py
@@ -2,68 +2,55 @@
 
 
 import cv2
-import MySQLdb
 import numpy as np
 from cv2 import imread, imshow
 import LSH
-import json
-import datetime
-
-starttime = datetime.datetime.now()
-conn=MySQLdb.connect (
-    host='42.121.114.70',
-    port=3306,
-    user='cccoi',
-    passwd='cccoiers',
-    db='buildingdetect',
-    )
-cur=conn.cursor();
 
 result = []
 #image = cv.LoadImage("target.jpg", 1)
 
 lsh=LSH.LSH(100,128,table_num=5)
-sift=cv2.SIFT()
+kp=[[] for i in range(155)]
+sift=cv2.SIFT(2000)
+for i in range(1, 155):
+    print i
+    image1 = cv2.imread(("/Users/mcdreamy/Documents/test2/%d.jpg" % i), 1)
+    x=0
+    image1=cv2.resize(image1,(0,0),fx=0.4,fy=0.4)
+    tkp,des=sift.detectAndCompute(image1,None)
+    print len(tkp)
+    kp[i-1].append(tkp)
+    for vec in des:
+        #print vec
+        lsh.index(vec,i*100000+x)
+        x=x+1
+while 1:
+    file_name=raw_input("please input the file name");
+    image=cv2.imread(file_name)
 
-#insert into database
-#have done in 2015/3/26
-#for i in range(5,10):
-#    print i
-#    image1 = cv2.imread(("%d.jpg" % i), 1)
-#    h,w,x=image1.shape
-#    #have done insert all
-#    #cur.execute("insert into image(buildingid,buildingname,filename,height,width) values('%d','build%d','E:\\\%d.jpg','%d','%d');" %(i,i,i,h,w))
-#    kp,des=sift.detectAndCompute(image1,None)
-#    for j in range(len(des)):
-#        x=json.dumps(des[j].tolist())
-#        vec = des[j]
-#        #cur.execute("insert into keypoint (pictureid,positionx,positiony,size,orient,localdescriptor) values('%d','%f','%f','%f','%f','%s');" % (i,kp[j].pt[0],kp[j].pt[1],kp[j].size,kp[j].angle,x)) 
-#        #cur.execute("SELECT LAST_INSERT_ID();")
-#        #conn.commit()
-#        keypointid=cur.fetchone()[0]
-#        #print "done"
-#        lsh.index(vec,'%d' % i,keypointid)
-#        lsh.index(vec,'%d' % i,0)
+    tkp,des=sift.detectAndCompute(image,None)
+    x=0
+    count=[0]*155
+    former = [[] for i in range(155)];
+    latter = [[] for i in range(155)]
+    for vec in des:
+        a=lsh.query(vec, 1)
+        if a:
+            ind = a[0][0][1];
+            img_no=ind/100000;kp_no=ind%100000;
+        #print img_no,kp_no,len(kp[img_no-1])
+       # print kp[img_no-1]
+            count[img_no-1]=count[img_no-1]+1
+            former[img_no-1].append(tkp[x].pt)
+            latter[img_no-1].append(kp[img_no-1][0][kp_no-1].pt)
+        x=x+1
+        #print count
 
-#print 'done'
-endtime = datetime.datetime.now()
-interval=(endtime - starttime).seconds
-print "time1",interval
-image=cv2.imread('target.png')
-rows,cols = image.shape[0],image.shape[1]
-M=np.float32(([1,0,100],[0,1,50]))
-img=cv2.warpAffine(image,M,(cols,rows))
-kp,des=sift.detectAndCompute(image,None)
-print len(des)
-endtime = datetime.datetime.now()
-interval=(endtime - starttime).seconds
-print "time2",interval
-for vec in des:
-    a=lsh.query(vec, 1)
-    print "1"
-    if a:
-        print a[0][0][1],a[0][1]
-endtime = datetime.datetime.now()
-interval=(endtime - starttime).seconds
-print "time1",interval
-#conn.commit()
+
+    for i in range(155):
+        if count[i]>3:
+            H,m = cv2.findHomography(np.float32(former[i]).reshape(-1,1,2),np.float32(latter[i]).reshape(-1,1,2),cv2.RANSAC);
+    i=count.index(max(count))
+    H,m = cv2.findHomography(np.float32(former[i]).reshape(-1,1,2),np.float32(latter[i]).reshape(-1,1,2),cv2.RANSAC);
+    print i,m
+    
